8131fe81fc9dcd5f7307094de099dc93cc0a4d82
nithin.bairi <nithyatech14@gmail.com>
2019-02-14 16:44:25 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'  #see the comment above.
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-
+STATICFILES_DIRS = ("D:/django/inventateq/blog/static/css",)
 
 
 
